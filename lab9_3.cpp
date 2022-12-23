dfe6397f57f73062813a745b75ce7ca42d95c6dd
fear <corefea@gmail.com>
2022-12-23 16:37:55 +0700

    9_3 completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,59 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    double a,h,b;
+    cout << "Enter your age: ";
+    cin >> a;
+    if(a<=25)
+    {   cout << "Enter your height: ";
+        cin >> h;
+        if(h <100)
+        {
+            cout << "Your character = Chopper"; 
+        }
+        else if(h <180)
+        { cout << "Your character = Usopp"; 
+        }
+        else if(h >=180)
+        {
+            cout <<"Enter your bounty: ";
+            cin >>b;
+                if(b > 1100000000)
+                {
+                    cout  << "Your character = Zoro"; 
+                }
+                else
+                {
+                    cout  << "Your character = Sanji";
+                }
+        }
+        
+    }
+    else if(a<=60)
+    {
+         cout <<"Enter your bounty: ";
+         cin >>b;
+         if(b>500000000)
+         {
+            cout <<  "Your character = Jinbe";
+         }
+         else
+         {
+            cout <<  "Your character = Franky";
+         }
+    }
+    else if(a>=60)
+    {
+        cout <<  "Your character = Brook";
+    }
+
+
+
+
+}
